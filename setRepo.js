230d0d1789c451c4602ce44ef0b47d560b123fdb
Katuula Joel <joel@tunga.io>
2020-02-16 03:19:00 +0300

    add ability to change directory

--- setRepo.js
+++ setRepo.js
@@ -1,18 +1,53 @@
 #!/usr/bin/env node
 const readline = require("readline");
+const fs = require('fs'); // todo: manage file creation in separate.
+const process = require('process'); // todo: manage this in separate file.
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+var repository = null
+var repository = null
+var directory = null
+
+/**
+ * Read initial information on your user.
+ * @param repo User repository where they want to set the new project
+ * @param fwork the javascript framework that the user wants to use
+ */
 rl.question("Git Repository ? ", function(repo) {
-    rl.question("Frontend Framework ? ", function(framework) {
-        console.log(`${repo}, is repo and framework ${framework}`);
-        rl.close();
+    rl.question("Frontend Framework ? ", function(fwork) {
+        rl.question("Application Name ? ", function(dir) {
+            repository = repo
+            framework = fwork
+            directory = dir
+            rl.close();
+        });
     });
 });
 
 rl.on("close", function() {
-    console.log("\nBYE BYE !!!");
+    if(repository && framework){
+
+        if (!fs.existsSync(directory)){
+            fs.mkdirSync(directory);
+
+            try {
+                process.chdir(directory);
+                
+            }
+            catch (err) {
+                console.log('chdir: ' + err);
+            }
+
+        }else{
+            console.log('failed to create directory')
+        }
+
+        var react_repo = 'https://github.com/katuulajoel/react-temp.git'
+
+        console.log(`Repo: ${repository} and framework is ${framework}`)
+    }
     process.exit(0);
 });
